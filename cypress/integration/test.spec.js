84c910eeb3a77d2ddfe45b1edf177ed4c6b8b540
Maisha Ahmed <58888586+maisha-ahmed@users.noreply.github.com>
2021-09-25 17:05:20 -0400

    Update test.spec.js

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -4,7 +4,7 @@ describe('Heading', () => {
 
         cy.get('h1')
             .invoke('text')
-            .should("equal", "My Awesome Webb Aplication")
+            .should("equal", "My Awesome Web Application")
     });
 
 });
